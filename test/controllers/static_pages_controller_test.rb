4093e0729ce52c8635f72aa5886de2174f83d48d
suzupy <suzuki.n@pepabo.com>
2015-06-10 13:10:28 +0900

    Add title tests for static_pages (red)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
