3038be44796cec52c85f1da5d91ef485823ff722
suzupy <suzuki.n@pepabo.com>
2015-06-10 13:14:48 +0900

    Update static_pages test (red)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -16,5 +16,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
