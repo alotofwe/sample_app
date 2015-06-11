10c46823531c8f23e19d4b1f17c9a575ee83f3c2
suzupy <suzuki.n@pepabo.com>
2015-06-11 16:45:11 +0900

    User.mailが大文字を全て小文字に変換して保存される機能のテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -52,6 +52,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
