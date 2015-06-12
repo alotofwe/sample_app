c362642ad4706a27537cb916f7bc3e20df27b19e
suzupy <suzuki.n@pepabo.com>
2015-06-12 14:05:36 +0900

    signupで不正な値を入れた時にエラーが出ているかのテスト追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,4 +11,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
   end
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
 end
