16af14beae8cee30164b79931d6013e3139d3e7c
suzupy <suzuki.n@pepabo.com>
2015-06-15 13:19:36 +0900

    remember_tokenに正しい値が入っているかどうかのテストを追記 (assignsが使     用できるよう@を使用)

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,18 +23,17 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 
-  # Returns the hash digest of the given string.
-  def digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-      BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
-
-  # Returns a random token.
-  def new_token
-    SecureRandom.urlsafe_base64
-  end
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
 
