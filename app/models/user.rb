22e95ebfa516bcd63ee88a6ab0998ce4b5e3e01d
suzupy <suzuki.n@pepabo.com>
2015-06-15 13:11:14 +0900

    UserのクラスメソッドをUser.hogeからclass<<selfの記述方法に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,21 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  class << self
+
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  def new_token
     SecureRandom.urlsafe_base64
   end
+  end
+
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
