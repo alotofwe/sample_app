bf9b401dcd44f91be4bcba4968bd562dd2f57ed1
suzupy <suzuki.n@pepabo.com>
2015-06-16 17:58:34 +0900

    パスワードリセット要求後，2時間以上経過時にリンク先を訪れた際に，期限が切れている旨をflashで通知しているかどうかのテストを追記 (expireという文字と共に通知しているべき)

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -38,24 +38,39 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_select "input[name=email][type=hidden][value=?]", user.email
     # Invalid password & confirmation
     patch password_reset_path(user.reset_token),
-          email: user.email,
-          user: { password:              "foobaz",
-                  password_confirmation: "barquux" }
-    assert_select 'div#error_explanation'
-    # Empty password
-    patch password_reset_path(user.reset_token),
-          email: user.email,
-          user: { password:              "",
-                  password_confirmation: "" }
-    assert_not flash.empty?
-    assert_template 'password_resets/edit'
-    # Valid password & confirmation
-    patch password_reset_path(user.reset_token),
+      email: user.email,
+      user: { password:              "foobaz",
+              password_confirmation: "barquux" }
+      assert_select 'div#error_explanation'
+      # Empty password
+      patch password_reset_path(user.reset_token),
+        email: user.email,
+        user: { password:              "",
+                password_confirmation: "" }
+        assert_not flash.empty?
+        assert_template 'password_resets/edit'
+        # Valid password & confirmation
+        patch password_reset_path(user.reset_token),
           email: user.email,
           user: { password:              "foobaz",
                   password_confirmation: "foobaz" }
-    assert is_logged_in?
-    assert_not flash.empty?
-    assert_redirected_to user
+          assert is_logged_in?
+          assert_not flash.empty?
+          assert_redirected_to user
+  end
+
+  test "expired token" do
+    get new_password_reset_path
+    post password_resets_path, password_reset: { email: @user.email }
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+      email: @user.email,
+      user: { password:              "foobar",
+              password_confirmation: "foobar" }
+      assert_response :redirect
+      follow_redirect!
+      assert_match /expired/i, response.body
   end
 end
