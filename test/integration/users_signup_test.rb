41b04bbdf3c7baafc0c1665dc94ae35a495998fb
suzupy <suzuki.n@pepabo.com>
2015-06-22 12:43:49 +0900

    conflictミス修正

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -47,17 +47,4 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     assert_not flash.empty?
   end
-
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: { name:  "",
-                               email: "user@invalid",
-                               password:              "foo",
-                               password_confirmation: "bar" }
-    end
-    assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
-  end
 end
