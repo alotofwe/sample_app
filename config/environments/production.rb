b73ae7d711040684b568b4eca71841aa890554a2
suzupy <suzuki.n@pepabo.com>
2015-07-10 15:42:59 +0900

    mailのhost変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'rails-tutorial-sample-suzupy.herokuapp.com'
+  # host = 'rails-tutorial-sample-suzupy.herokuapp.com'
+  host = 'localhost:8080'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     # :address        => 'smtp.gmail.com',
