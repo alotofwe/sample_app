f343c18871e30da5bef0299aacab482ef2d42687
suda-k <suda.k@pepabo.com>
2015-08-04 18:50:37 +0900

    投稿画像のasset_hostをstorageサーバへ向ける

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,13 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.action_controller.asset_host = Proc.new { |source|
-    if source.starts_with?('/uploads')
-     ENV['STORAGE_HOST']
-   else
-     ENV['ASSET_HOST']
-   end
-  }
+  config.action_controller.asset_host = ENV['ASSET_HOST']
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
